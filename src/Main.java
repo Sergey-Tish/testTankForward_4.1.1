ab6cd980a332914885503887d43632521994486e
Sergey Tishchenko <seti.volch@gmail.com>
2023-01-09 22:11:23 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,47 @@
+public class Main {
+
+
+    public static void main(String[] args) {
+/*
+Make (add code to class Tank),
+with fuel and different constructors
+*/
+
+        // At (0;0) fuel=100
+        Tank justTank = new Tank();
+        Tank.printTankCreate();
+        // At (10;10) fuel=100
+        Tank anywareTank = new Tank(10, 10);
+        Tank.printTankCreate();
+        // At (20;30) fuel=200
+        Tank customTank = new Tank(20, 30, 200);
+        Tank.printTankCreate();
+        System.out.println("Total tanks we have: " + justTank.getTankCount() + " pcs");
+
+        justTank.goForward(200);
+        justTank.printPosition();
+        anywareTank.goBackward(-200);
+        anywareTank.printPosition();
+        customTank.goForward(201);
+        customTank.printPosition();
+        Tank tank = new Tank();
+        tank.goForward(10);
+        tank.printPosition();
+        tank.turnRight();
+        tank.goForward(50);
+        tank.printPosition();
+        tank.turnLeft();
+        tank.goBackward(500);
+        tank.printPosition();
+
+/*
+This fragment of code has to output
+
+The Tank is at 100, 0 now.
+The Tank is at 110, 10 now.
+The Tank is at 220, 30 now.
+
+*/
+    }
+
+}
---
+++ src/Tank.java
@@ -0,0 +1,121 @@
+public class Tank {
+    private int coordinateX;
+    private int coordinateY;
+    private int route = 0; // 1 = up ; 2 = right; 3 = down ; 4 = left ;
+    private int fuel = 100;
+
+    private final String tankNumber = "" + (tankCount + 1);
+    private final String tankModel = "T34";
+
+
+    private static int tankCount = 0;
+
+    void goForward(int pathWay) {
+        if (route == 0) changeX(pathWay);
+        if (route == 3) changeY(pathWay);
+        if (route == 2) changeX(-pathWay);
+        if (route == 1) changeY(-pathWay);
+        routeCheck();
+    }
+
+    void goBackward(int pathWay) {
+        if (route == 0) changeX(-pathWay);
+        if (route == 3) changeY(-pathWay);
+        if (route == 2) changeX(pathWay);
+        if (route == 1) changeY(pathWay);
+        routeCheck();
+    }
+
+    void printPosition() {
+        System.out.println("The Tank " + tankModel + "-" + tankNumber + " is at " + coordinateX + ", " + coordinateY + " now.");
+    }
+
+    void turnLeft() {
+        --route;
+        if (route < 0) {
+            route = 3;
+        }
+    }
+
+    void turnRight() {
+        ++route;
+        if (route > 3) {
+            route = 0;
+        }
+    }
+
+    void changeX(int i) {
+        if (i < 0) {
+            if (Math.abs(i) <= fuel) {
+                coordinateX += i;
+                fuel -= Math.abs(i);
+            } else {
+                coordinateX -= fuel;
+                fuel -= fuel;
+            }
+        } else {
+            if (Math.abs(i) <= fuel) {
+                coordinateX += i;
+                fuel -= Math.abs(i);
+            } else {
+                coordinateX += fuel;
+                fuel -= fuel;
+            }
+        }
+    }
+
+    void changeY(int i) {
+        if (i < 0) {
+            if (Math.abs(i) <= fuel) {
+                coordinateY += i;
+                fuel -= Math.abs(i);
+            } else {
+                coordinateY -= fuel;
+                fuel -= fuel;
+            }
+        } else {
+            if (Math.abs(i) <= fuel) {
+                coordinateY += i;
+                fuel -= Math.abs(i);
+            } else {
+                coordinateY += fuel;
+                fuel -= fuel;
+            }
+        }
+    }
+
+    void routeCheck() {
+        if (route != 0 && route != 1 && route != 2 && route != 3) {
+            System.out.println("Route is error!");
+        }
+    }
+
+    public Tank() {
+        ++tankCount;
+    }
+
+    public Tank(int coordinateX, int coordinateY) {
+        this();
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+    }
+
+    public Tank(int coordinateX, int coordinateY, int fuel) {
+        this();
+        this.coordinateX = coordinateX;
+        this.coordinateY = coordinateY;
+        this.fuel = fuel;
+    }
+
+    public static void printTankCreate() {
+        System.out.println("Create new tank");
+    }
+
+    public String getTankNumber() {
+        return tankNumber;
+    }
+
+    public int getTankCount() {
+        return tankCount;
+    }
+}
